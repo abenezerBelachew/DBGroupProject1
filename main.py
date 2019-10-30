0bf82b1102f85d87c8948a65349840038551f73b
Abenezer Belachew <belachew@ualberta.ca>
2019-10-30 02:07:23 -0600

    Implemented get_database() - It gets the database from the command line argument and sends it to main()

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 import sqlite3
 import sys
+import program_messages as pm
+from os import path
+
+
+
+def get_database():
+    # Checks if there is an argument provided
+    if len(sys.argv) > 1:
+        # Checks if it's a valid database
+        if (path.exists(sys.argv[1])):
+            try:
+                # Connect to and return the database
+                database = sqlite3.connect(sys.argv[1])
+                return database
+            except:
+                print(pm.something_went_wrong)
+                sys.exit()
+        else:
+            print(pm.db_not_found)
+            sys.exit()
+    else:
+        print(pm.provide_db_name)
+        sys.exit()
 
 def main():
     """ Connects to database and other modules."""
-    pass
+    database = get_database()
+    cursor = database.cursor()
+   
+    # Just for test
+    cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+    print(cursor.fetchall())
+    # -------------------
 
 
 if __name__ == '__main__':
