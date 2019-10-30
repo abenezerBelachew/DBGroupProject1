9ba1b88f076f60acdf95fd5d397db2b358075fe8
Abenezer Belachew <belachew@ualberta.ca>
2019-10-30 00:36:41 -0600

    forTesting.py can be used for simple python tests you might have while working on the project

---
+++ forTesting.py
@@ -0,0 +1,10 @@
+help_commands = ["RegBir to Register a Birth", 
+            "RegMar to Register a Marriage",
+            "RenVeh to Renew a vehicle",
+            "ProBil to Process a bill of sale",
+            "ProPay to Process a payment",
+            "DriAbs to Get a driver abstract",
+            "IsuTic to Issue a ticket",
+            "CarOwn to Find a car owner"]
+
+
