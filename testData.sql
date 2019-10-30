55d96fb0c6ef7c383c864b5f12cb8f4e0587db53
Abenezer Belachew <belachew@ualberta.ca>
2019-10-30 12:55:52 -0600

    Additional test data

---
+++ testData.sql
@@ -0,0 +1,7 @@
+ insert into users values ('user1', 'password123', 'a', 'FN User1', 'LN User1', 'Alberta');
+ insert into users values ('user2', 'password', 'a', 'FN User2', 'LN User2', 'Addis');
+ insert into users values ('user3', '123password', 'a', 'FN User3', 'LN User3', 'New York');
+ insert into users values ('officer1', 'call911', 'o', 'FN Officer1', 'LN Officer1', 'New York');
+ insert into users values ('officer2', 'testingpass', 'o', 'FN Officer2', 'LN Officer2', 'Alberta');
+ insert into users values ('officer3', 'plants', 'o', 'FN Officer3', 'LN Officer3', 'Addis');
+ 
