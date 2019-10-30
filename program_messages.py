36ff582de8a0d8e040a0e7fa6e9513a9a58fee6c
Abenezer Belachew <belachew@ualberta.ca>
2019-10-30 02:09:05 -0600

    This is for strings, list and dictionaries. Useful to make the code clean.

---
+++ program_messages.py
@@ -0,0 +1,25 @@
+# Commands related
+provide_argument = "Please enter a command. \nType help to see list of commands."
+provide_valid_argument = "Please provide a valid argument. \nType help to see a list of available commands."
+help_commands = ["RegBir to Register a Birth", 
+            "RegMar to Register a Marriage",
+            "RenVeh to Renew a vehicle",
+            "ProBil to Process a bill of sale",
+            "ProPay to Process a payment",
+            "DriAbs to Get a driver abstract",
+            "IsuTic to Issue a ticket",
+            "CarOwn to Find a car owner",
+            "Help to see available commands",
+            "Quit to quit",]
+lst_of_commands = ["RegBir", "RegMar", "RenVeh", "ProBil",
+                    "ProPay", "DriAbs", "IsuTic", "CarOwn",
+                    "help", "quit"]
+
+# Database related
+db_not_found = "Database not found."
+provide_db_name = "Please provide a database name."
+
+
+# General
+quit_message = "Closing everything..bye"
+something_went_wrong = "Something went wrong"
