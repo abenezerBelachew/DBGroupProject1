d2a7319ce1b96e80906cb0cd77b5bef2e986c34b
Abenezer Belachew <belachew@ualberta.ca>
2019-10-30 02:06:26 -0600

    Create your own file if you want to test something

--- forTesting.py
+++ forTesting.py
@@ -1,3 +1,5 @@
+import sys
+import program_messages as pm
 help_commands = ["RegBir to Register a Birth", 
             "RegMar to Register a Marriage",
             "RenVeh to Renew a vehicle",
@@ -8,3 +10,34 @@ help_commands = ["RegBir to Register a Birth",
             "CarOwn to Find a car owner"]
 
 
+print(sys.argv[1])
+print(type(sys.argv[1]))
+print(len(sys.argv))
+
+if (len(sys.argv) > 1):
+    pass
+else:
+    print("Please enter a command.\nType help to see list of commands.")
+
+if sys.argv[1] == "help":
+    print(':)')
+
+
+# For the arguments
+# Checks if there is an argument provided
+    if len(sys.argv) > 1:
+        # Checks if it's a valid argument
+        if sys.argv[1] in pm.lst_of_commands:
+            # If command is help, list all available commands
+            if sys.argv[1] == "help":
+                for help_command in ps.help_commands:
+                    print(f'|- {help_command} ')
+            # if quit, close database and application
+            elif sys.argv[1] == "quit":
+                # TODO: Quit the database
+                print(pm.quit_message)
+                sys.exit()
+        else:
+            print(pm.provide_valid_argument)
+    else:
+        print(pm.provide_argument)
