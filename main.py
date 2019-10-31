162319da23f74c6952b8fdfc530fce305c264b67
Abenezer Belachew <belachew@ualberta.ca>
2019-10-31 17:39:17 -0600

    Implemented register_birth

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def login(database):
     if uid in login_credentials.keys():
         # If the uid matches its associated pwd, return True
         if login_credentials[uid] == pwd:
-            active_user = uid
-            return True
+            # active_user = uid
+            return (True, uid)
         else:
             print(pm.password_incorrect)
     else:
@@ -40,10 +40,10 @@ def register_birth(database, user):
     """The agent should be able to register a birth by providing the first name, the last name, 
     the gender, the birth date, the birth place of the newborn, as well as the first and last 
     names of the parents."""
-    regno = randrange(1001, 9899)
-    fname = input("First Name: ")
-    lname = input("Last Name: ")
-    gender = str(input("Gender(F/M): ").capitalize())
+    regno = randrange(1001, 9867699)
+    fname = str(input("First Name: "))
+    lname = str(input("Last Name: "))
+    gender = str(input("Gender(F/M): ").upper())
     birth_date = str(input("Birth Date(YYYY-MM-DD): "))
     birth_place = str(input("Birth Place: "))
     f_fname = str(input("Father's First Name: "))
@@ -51,7 +51,31 @@ def register_birth(database, user):
     m_fname = str(input("Mother's First Name: "))
     m_lname = str(input("Mother's Last Name: "))
     regdate = datetime.today().strftime('%Y-%m-%d')
-    # regplace = # write a query for birth from the user
+
+    print("User: ", user)
+    c = database.cursor()
+    c.execute("SELECT city FROM users WHERE uid=?", (user,))
+    user_city = c.fetchone()
+    city = user_city[0]
+    regplace = city
+
+    
+
+    try:
+        c.execute("SELECT phone, address FROM persons p JOIN births b ON (p.fname, p.lname) = (b.m_fname, b.m_lname) WHERE m_fname = ? AND m_lname = ?", (m_fname, m_lname, ))
+        phone = c.fetchone()[0]
+        address = c.fetchone()[1]
+    except:
+        print(pm.something_went_wrong)
+        sys.exit()
+    
+    reg_birth_data = [regno, fname, lname, regdate, regplace, gender, f_fname, f_lname, m_fname, m_lname]
+    person_data = [fname, lname, birth_date, birth_place, address, phone]
+
+    c.execute("INSERT INTO births VALUES (?,?,?,?,?,?,?,?,?,?)", reg_birth_data)
+    c.execute("INSERT INTO persons VALUES (?,?,?,?,?,?)", person_data)
+    
+    # TODO: If parents not in db, system should get fn, ln, bd, bp, address, phone
     
     
 
@@ -80,9 +104,12 @@ def main():
     """ Connects to database and other modules."""
     database = get_database()
     # If the user can log in
-    if login(database):
+    status, active_user = login(database)
+    if status:
         print(pm.logged_in)
+        print("User in main: ", active_user)
         
+
         for command in pm.help_commands:
             # list all available commands
             print(command)
@@ -101,7 +128,8 @@ def main():
 
         elif command == "REGBIR":
             # for registering birth
-            print("Registering birth")
+            print(pm.register_birth)
+            register_birth(database, active_user)
 
     else:
         print("Not logged in.")    
