2524d7725a2c613148dd567ddd43406c6ce7c209
Abenezer Belachew <belachew@ualberta.ca>
2019-10-30 20:43:53 -0600

    Implemented login

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 import sqlite3
 import sys
 import program_messages as pm
+import queries as q
 from os import path
+
+
+login_credentials = {}
+
+def login(database):
+    """ Returns true if login credentials work. """
+    c = database.cursor()
+    c.execute(q.get_uid_pwd)
+    uid_pwd = c.fetchall()
+    # Assign every username, password combination to the login_credentials dictionary
+    for u_p in uid_pwd:
+        login_credentials[u_p[0]] = u_p[1]
+
+    # Ask user for uid and pwd
+    uid = str(input("Your uid: "))
+    pwd = str(input("Your password: "))
+
+
+    if uid in login_credentials.keys():
+        # If the uid matches its associated pwd, return True
+        if login_credentials[uid] == pwd:
+            return True
+        else:
+            print(pm.password_incorrect)
+    else:
+        # TODO: Allow user to try again
+        print(pm.uid_not_exist)
+    
+    # c.execute(q.get_uid_pwd)
+    # uid_pwd = c.fetchall()
+    # for uid, pwd in uid_pwd:
+    #     login_credentials[uid] = pwd
+    
 
 
 
@@ -27,12 +61,13 @@ def get_database():
 def main():
     """ Connects to database and other modules."""
     database = get_database()
-    cursor = database.cursor()
+    # If the user can log in
+    if login(database):
+        print("You're logged in.")
+    else:
+        print("Not logged in.")
    
-    # Just for test
-    cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
-    print(cursor.fetchall())
-    # -------------------
+    
 
 
 
